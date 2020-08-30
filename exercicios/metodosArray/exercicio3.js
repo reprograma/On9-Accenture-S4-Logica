82da0fa481e8e27ddc4b803be03e8c7cd6d64cd8
Josélia L. Costa <joselialcosta@gmail.com>
2020-08-30 11:19:26 -0300

    Adds console log on exercise 3

--- exercicios/metodosArray/exercicio3.js
+++ exercicios/metodosArray/exercicio3.js
@@ -1,15 +1,20 @@
 let sorvetes = [
-    "Coco",
-    "Maracujá",
-    "Abacaxi",
-    "Baunilha",
-    "Tangerina",
-    "Doce de Leite",
-    "Iogurte de frutas vermelhas",
-    "Melancia",
-    "Chocolate",
-    "Chocolate Belga",
-    "Torta de limão",
-  ];
-
-  sorvetes.splice(6,1, "Paçoquita", "Passas ao rum");
+  "Coco",
+  "Maracujá",
+  "Abacaxi",
+  "Baunilha",
+  "Tangerina",
+  "Doce de Leite",
+  "Iogurte de frutas vermelhas",
+  "Melancia",
+  "Chocolate",
+  "Chocolate Belga",
+  "Torta de limão",
+];
+console.log("Antes");
+console.log("----------");
+console.log(sorvetes);
+console.log("Depois");
+console.log("----------");
+sorvetes.splice(6, 1, "Paçoquita", "Passas ao rum");
+console.log(sorvetes);
