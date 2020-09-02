f9868284bbfea244640e2a9fc25f0a6ea576a0e6
Jennyffer de Morais <jennyffernovaes@hotmail.com>
2020-09-02 14:25:54 -0300

    adicionar resolução do exercício 2

---
+++ entrega/exercicio-2.js
@@ -0,0 +1,11 @@
+/* Função disco da Xuxa, o retorno.
+Use a documentação MDN para determinar quais destes métodos seria o melhor para reverter elementos nesta array:
+
+reverseMe = ["h", "e", "l", "l", "o"]; */
+
+const reverseMe = ["h", "e", "l", "l", "o"];
+
+const reversed = reverseMe.reverse();
+console.log('Texto revertido:', reversed);
+
+
