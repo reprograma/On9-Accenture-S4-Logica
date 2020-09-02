bde7c5112fde8650816e4fa2e3551feb1a1aa159
Fabi <fabicaneyu@gmail.com>
2020-09-02 09:41:32 -0300

    Exercício 2 finalizado

---
+++ entrega/exerciciosFabi/exercicio2.js
@@ -0,0 +1,19 @@
+/* 
+2. Função disco da Xuxa, o retorno.
+
+Use a [documentação MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array) 
+para determinar quais destes métodos seria o melhor para reverter elementos nesta array:
+
+```javascript
+reverseMe = ["h", "e", "l", "l", "o"];
+``` */
+
+//Array.reverse()
+
+const reverseMe = ["h","e","l","l","o"];
+console.log("Array1: ", reverseMe);
+
+const reversed = reverseMe.reverse();
+console.log("Array 1 revertido: ", reversed);
+
+
