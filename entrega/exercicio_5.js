4f624b7a846dd20139bb66c35b0006f5cbae37ce
cyntiagoulart <cyntiartg@gmail.com>
2020-09-03 18:04:35 -0300

    Ajuste de mensagem de output e conversão de números do exercício 5.

--- entrega/exercicio_5.js
+++ entrega/exercicio_5.js
@@ -12,31 +12,33 @@ function cadastramento() {
 		'Digite o seu nome completo: '
 	);
 	apresentacao.idade = readlineSync.question('Digite a sua idade: ');
-	apresentacao.altura = readlineSync.question('Digite a sua altura: ');
+	apresentacao.altura = readlineSync.question(
+		'Digite a sua altura em centímetros: '
+	);
 	apresentacao.metrosCaminhados = readlineSync.question(
 		'Digite quantos metros caminhou: '
 	);
 
 	let nome = apresentacao.nomeCompleto;
-	let idade = apresentacao.idade;
-	let altura = apresentacao.altura;
-	let distancia = apresentacao.metrosCaminhados;
+	let idade = Number(apresentacao.idade);
+	let altura = Number(apresentacao.altura);
+	let distancia = Number(apresentacao.metrosCaminhados);
 
 	if (idade <= 1 && distancia <= 1) {
 		console.log(
-			`Olá, eu sou ${nome}, tenho ${idade} ano, minha é ${altura} e só hoje, eu já caminhei ${distancia} metro!`
+			`Olá, eu sou ${nome}, tenho ${idade} ano, minha altura é ${altura}cm e só hoje, eu já caminhei ${distancia} metro!`
 		);
 	} else if (idade <= 1) {
 		console.log(
-			`Olá, eu sou ${nome}, tenho ${idade} ano, minha é ${altura} e só hoje, eu já caminhei ${distancia} metros!`
+			`Olá, eu sou ${nome}, tenho ${idade} ano, minha altura é ${altura}cm e só hoje, eu já caminhei ${distancia} metros!`
 		);
 	} else if (distancia <= 1) {
 		console.log(
-			`Olá, eu sou ${nome}, tenho ${idade} anos, minha é ${altura} e só hoje, eu já caminhei ${distancia} metro!`
+			`Olá, eu sou ${nome}, tenho ${idade} anos, minha altura é ${altura}cm e só hoje, eu já caminhei ${distancia} metro!`
 		);
 	} else {
 		console.log(
-			`Olá, eu sou ${nome}, tenho ${idade} anos, minha é ${altura} e só hoje, eu já caminhei ${distancia} metros!`
+			`Olá, eu sou ${nome}, tenho ${idade} anos, minha altura é ${altura}cm e só hoje, eu já caminhei ${distancia} metros!`
 		);
 	}
 }
