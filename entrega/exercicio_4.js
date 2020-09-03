d09686484e221b9c8abbb4426cf98fe23bdf6baa
cyntiagoulart <cyntiartg@gmail.com>
2020-09-03 18:17:22 -0300

    Alteração de código para conversão de números no exercício 4.

--- entrega/exercicio_4.js
+++ entrega/exercicio_4.js
@@ -11,19 +11,22 @@ function cadastramento() {
 	cadastro.nomeCompleto = readlineSync.question(
 		'Digite o seu nome completo: '
 	);
-	console.log(cadastro.nomeCompleto);
-
 	cadastro.idade = readlineSync.question('Digite a sua idade: ');
-	console.log(cadastro.idade);
-
 	cadastro.altura = readlineSync.question(
 		'Digite a sua altura em centímetros: '
 	);
-	console.log(cadastro.altura);
-
 	cadastro.metrosCaminhados = readlineSync.question(
 		'Digite quantos metros caminhou: '
 	);
-	console.log(cadastro.metrosCaminhados);
+
+	let nome = cadastro.nomeCompleto;
+	let idade = Number(cadastro.idade);
+	let altura = Number(cadastro.altura / 100);
+	let distancia = Number(cadastro.metrosCaminhados);
+
+	console.log(nome);
+	console.log(idade);
+	console.log(altura);
+	console.log(distancia);
 }
 cadastramento();
