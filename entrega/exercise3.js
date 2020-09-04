310fd88e12097b5e5264f779c7e558d0b0516517
Irla Andrade <paula.irla@gmail.com>
2020-09-04 10:05:08 -0300

    melhorando o exercício 3

--- entrega/exercise3.js
+++ entrega/exercise3.js
@@ -1,13 +1,9 @@
 /* Crie uma função que retorna a soma dos elementos de um array. Exemplo:
     //soma = 1 + 2 + 3 + 4 + 5 = 15 */
 
-    let sumElements = [2, 3, 5, 8, 10, 4, 7];
+let sumElements = [2, 3, 5, 8, 10, 4, 7];
 
-    function sum(array) {
-      let result = 0;
-      for (index = 0; index < array.length; index++) {
-        result += array[index];
-      }
-      return result;
-    }
-    console.log(sum(sumElements));
+let result = sumElements.reduce(function (acumulador, valorAtual) {
+  return acumulador + valorAtual;
+});
+console.log(result);
