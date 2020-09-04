562a398073e208b056f1c7660c7408bb0fbddf2f
Manoela <manoela.pala@gmail.com>
2020-09-04 00:07:46 -0300

    editando_exercicio_05

--- exercicioS4_05.js
+++ exercicioS4_05.js
@@ -6,15 +6,19 @@ let pessoa = {
     altura: 0,
     metrosCaminhadosDia: 0,
     apresentacao:function(){
-    if(pessoa.idade == 1 && pessoa.metrosCaminhadosDia == 1){
-        return "Olá, eu sou " + pessoa.nome + " tenho " + pessoa.idade + " ano, minha altura é " + pessoa.altura + " e só hoje, eu já caminhei " + pessoa.metrosCaminhadosDia + " metro!"
-    }if(pessoa.idade == 1){
-        return "Olá, eu sou " + pessoa.nome + " tenho " + pessoa.idade + " ano, minha altura é " + pessoa.altura + " e só hoje, eu já caminhei " + pessoa.metrosCaminhadosDia + " metros!"
-    }if(pessoa.metrosCaminhadosDia == 1){
-        return "Olá, eu sou " + pessoa.nome + " tenho " + pessoa.idade + " anos, minha altura é " + pessoa.altura + " e só hoje, eu já caminhei " + pessoa.metrosCaminhadosDia + " metro!"
+        let idadeSingularPlural = "";
+        let metrosSingularPlural = "";
+    if(pessoa.idade == 1){
+        idadeSingularPlural = "ano";
     }else{
-        return "Olá, eu sou " + pessoa.nome + " tenho " + pessoa.idade + " anos, minha altura é " + pessoa.altura + " e só hoje, eu já caminhei " + pessoa.metrosCaminhadosDia + " metros!"
-    }   
+        idadeSingularPlural = "anos";
+    };
+    if(pessoa.metrosCaminhadosDia ==1){
+        metrosSingularPlural = "metro";
+    }else{
+        metrosSingularPlural = "metros";
+    }
+    return `Olá, eu sou ${pessoa.nome}, tenho ${pessoa.idade} ${idadeSingularPlural}, minha altura é ${pessoa.altura} e, só hoje, eu já caminhei ${pessoa.metrosCaminhadosDia} ${metrosSingularPlural}!` 
     },
     };
     
