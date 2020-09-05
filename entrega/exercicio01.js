84525e4c2ca9af093d93b54b34d91037a4468b4a
aline-carvalho <alinecarvalhomendes03@gmail.com>
2020-09-05 17:53:03 -0300

    Entrega dos exercicios 01, 02, 03, 04 e 05 da aula de logica

---
+++ entrega/exercicio01.js
@@ -0,0 +1,24 @@
+//1. James estava criando uma array com as cores do arco-íris, e ele esqueceu algumas cores. 
+//As cores padrão de um arco-íris são normalmente listadas nesta ordem:
+
+//javascript
+//let rainbow = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"];
+
+//mas James tinha isto:
+//`let rainbow = ["Red", "Orange", "Blackberry", "Blue"];`
+
+//Usando somente o método `splice()`, insira as cores que faltam na array e remova a cor "Blackberry", seguindo estes passos:
+
+//Remova "Blackberry"
+//Adicione "Yellow" e "Green"
+//Adicione "Purple"
+
+let rainbow = ["Red", "Orange", "Blackberry", "Blue"]
+  console.log("Antes");
+  console.log("----------");
+  console.log(rainbow);
+  console.log("Depois");
+  console.log("----------");
+  rainbow.splice(2, 1, "Yellow", "Green", "Purple");
+  console.log(rainbow);
+  
