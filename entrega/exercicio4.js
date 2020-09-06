11106e10c871262f47322f51c15652ec40f5a61c
Maria Aparecida Costa <maria.apcosta@outlook.com>
2020-09-05 21:34:40 -0300

    exercicios 4 semana

---
+++ entrega/exercicio4.js
@@ -0,0 +1,12 @@
+let CadastroPessoas = {
+    NomeCompleto: "Maria",
+    Idade: 24,
+    Altura: 167,
+    MetrosPercorridos: 1000,
+
+}
+
+console.log(CadastroPessoas.NomeCompleto);
+console.log(CadastroPessoas.Idade);
+console.log(CadastroPessoas.Altura);
+console.log(CadastroPessoas.MetrosPercorridos);
---
+++ entrega/exercicio5.js
@@ -0,0 +1,21 @@
+let health ={
+    Nome: "Maria",
+    Idade: 24,
+    Altura: 157,
+    MetrosPorDia:1000;
+apresentacao: function(age, running){
+    let ageSingularPlural;
+    if(age==1){
+        let ageSingularPlural = ano 
+    }
+    else{
+        let ageSingularPlural = anos 
+    }
+    if(running==1){
+        health.MetrosPorDia + running + metro  
+    }
+    return "Olá, eu sou a " + (health.Nome), " tenho " (health.Idade) + ageSingularPlural "anos, minha é " (health.Altura) " e só hoje, eu já caminhei " (health.MetrosPorDia) + ageSingularPlural + " metros!"
+}
+
+}
+console.log(health(apresentacao))
